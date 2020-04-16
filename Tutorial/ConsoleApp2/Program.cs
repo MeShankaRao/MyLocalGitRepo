51ce7fef4aceb9e32c3891c19dc9cc61a44b8de8
Rao, Shashank N (CT RDA DS AA DF PLT FH) <shashankn.rao@siemens.com>
2020-04-16 18:24:09 +0530

    More change

--- Tutorial/ConsoleApp2/Program.cs
+++ Tutorial/ConsoleApp2/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("New change");
+            Console.WriteLine("More change");
         }
     }
 }
